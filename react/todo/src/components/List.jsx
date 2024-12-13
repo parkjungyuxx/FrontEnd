f0803d9f372d70ab1d1db3049c0d14f204a2927d
Jungyu Park <parkjungyu09@gmail.com>
2024-12-14 02:18:18 +0900

    errorrrrr

--- react/todo/src/components/List.jsx
+++ react/todo/src/components/List.jsx
@@ -5,7 +5,9 @@ import { TodosContext } from "../context/TodosContext";
 import Modal from "./Modal";
 
 export default function List() {
-  const { todos, setTodos } = useContext(TodosContext);
+  const { todos, setTodos, isChecked, setIschecked, isEditing, setIsEditing } =
+    useContext(TodosContext);
+  const [editTitle, setEditTitle] = useState("");
   const [editText, setEditText] = useState("");
 
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,8 +18,10 @@ export default function List() {
     setTodos(copy);
   };
 
-  const handleEdit = (i) => {
-    const copy = [...todos];
+  const handleEditStart = (i) => {
+    const todo = todos[i];
+    const copy = [...todos, { ...todo, isEditing: true }];
+    setTodos(copy);
   };
 
   return (
@@ -27,8 +31,19 @@ export default function List() {
           <style.InputContainer>
             <input type="checkbox" />
             <style.ListTextContainer>
-              <p>제목: {todos[i].title}</p>
-              <p>할일: {todos[i].text}</p>
+              {todos[i].isEditing ? (
+                <EditForm
+                  editTitle={editTitle}
+                  editText={editText}
+                  setEditTitle={setEditTitle}
+                  setEditText={setEditText}
+                />
+              ) : (
+                <>
+                  <p>제목: {todos[i].title}</p>
+                  <p>할일: {todos[i].text}</p>
+                </>
+              )}
             </style.ListTextContainer>
           </style.InputContainer>
           <style.ButtonContainer>
@@ -39,13 +54,41 @@ export default function List() {
             >
               <MdDeleteOutline />
             </style.ListBtn>
-            <style.ListBtn onClick={()=>setIsModalOpen(!isModalOpen)}>
+            <style.ListBtn onClick={() => handleEditStart(i)}>
               <MdOutlineEdit />
             </style.ListBtn>
           </style.ButtonContainer>
         </style.Li>
       ))}
-      {isModalOpen && <Modal />}
     </style.Ul>
   );
 }
+
+const EditForm = ({ setEditText, setEditTitle, editText, editTitle }) => {
+  const { todos, setTodos, isChecked, setIschecked, isEditing, setIsEditing } =
+    useContext(TodosContext);
+
+  const finishEdit = (e, i) => {
+    e.preventDefault();
+    const todo = todos[i];
+    const copy = [...todos, { ...todo, title: editTitle, text: editText }];
+    setTodos(copy);
+  };
+  return (
+    <form onSubmit={finishEdit}>
+      <input
+        onChange={(e) => setEditTitle(e.target.value)}
+        value={editTitle}
+        type="text"
+        placeholder="제목을 수정해주세요"
+      />
+      <input
+        onChange={(e) => setEditText(e.target.value)}
+        value={editText}
+        type="text"
+        placeholder="내용을 수정해주세요"
+      />
+      <button>완료</button>
+    </form>
+  );
+};
